bc300003eaa9369f96444618b83d9f14d3a7b4a5
Jamie <phillipsja97@gmail.com>
2019-09-23 19:51:06 -0500

    working component of store card

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -4,9 +4,26 @@ const printToDom = (divId, toPrint) => {
 
 };
 
+const bookInfo = {
+  price: 24.99,
+  title: "Fifty Shades of Chicken",
+  image: "./assets/images/chicken.jpg"
+};
+
+const addToCartEvent = () => {
+ console.log('added to cart');
+}
+
 const makeStore = () => {
-    const domString = 'hi new store';
+    let domString = '<h2>Our only book:</h2>';
+    domString += '<p>Buy it now:</p>';
+    domString += `<h3>$${bookInfo.price}</h3>`;
+    domString += `<img src=${bookInfo.image} alt="book cover"/>`;
+    domString += '<button id="cart-button" class="btn btn-danger">Add To Cart</button>';
+
+
     printToDom('store-container', domString);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent)
 };
 
 export default { makeStore };
